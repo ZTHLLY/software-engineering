6675bff6e5cae56c3e9f413cf4a2f0e6c4f7b866
ZTHLLY <zzz18011940139@gmail.com>
2022-11-23 23:03:37 +0800

    1.1

---
+++ javascript/donate.js
@@ -0,0 +1,14 @@
+var info1=document.getElementById('info1');
+var info2=document.getElementById('info2');
+var info3=document.getElementById('info3');
+var info4=document.getElementById('info4');
+var info5=document.getElementById('info5');
+
+var reset=document.getElementById('reset');
+reset.onclick=function(){
+    info1.value='';
+    info2.value='';
+    info3.value='';
+    info4.value='';
+    info5.value='';
+}
